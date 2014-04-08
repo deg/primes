069d37ef63cd2c490accc7c4f9b5b007190a0e66
David Goldfarb <deg@degel.com>
2014-04-08 13:58:37 +0300

    Baby steps towards real code.

--- project.clj
+++ project.clj
@@ -13,7 +13,9 @@
                  [org.clojure/clojurescript "0.0-2156"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [om "0.4.1"]
-                 [com.facebook/react "0.8.0.1"]]
+                 [com.facebook/react "0.8.0.1"]
+                 ;; [TODO] Put back once cleaned up to prevent reader crash starting nrepl
+                 #_[degel/degel-clojure-utils "0.1.21" :exclusions [org.clojure/tools.reader]]]
   
   :plugins [[lein-cljsbuild "1.0.2"]]
 
--- src/cljs/primes/core.cljs
+++ src/cljs/primes/core.cljs
@@ -1,10 +1,23 @@
+;;; Copyright (c) 2014 David Goldfarb. All rights reserved.
+;;; Contact info: deg@degel.com
+;;;
+;;; The use and distribution terms for this software are covered by the Eclipse
+;;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which can
+;;; be found in the file epl-v10.html at the root of this distribution.
+;;; By using this software in any fashion, you are agreeing to be bound by the
+;;; terms of this license.
+;;;
+;;; You must not remove this notice, or any other, from this software.
+
+
 (ns primes.core.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [degel.primes.numbers :as nums]))
 
 (enable-console-print!)
 
-(def app-state (atom {:text "Hello world!"}))
+(def app-state (atom {:text (str (nums/group-signatures 100))}))
 
 (om/root
   (fn [app owner]

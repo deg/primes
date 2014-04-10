693cd4cb27624d7e75599434fbbfb94194670d6b
David Goldfarb <deg@degel.com>
2014-04-10 16:15:45 +0300

    Note the latest versions of dependencies. But, don't upgrade yet, since something is not stable, and I don't want to wrestle this right now.

--- project.clj
+++ project.clj
@@ -11,13 +11,18 @@
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2156"]
+                 #_[org.clojure/clojurescript "0.0-2202"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 #_[org.clojure/core.async "0.1.278.0-76b25b-alpha"]
                  [om "0.4.1"]
+                 #_[om "0.5.3"]
                  [com.facebook/react "0.8.0.1"]
+                 #_[com.facebook/react "0.9.0.1"]
                  ;; [TODO] Put back once cleaned up to prevent reader crash starting nrepl
-                 #_[degel/degel-clojure-utils "0.1.21" :exclusions [org.clojure/tools.reader]]]
+                 #_[degel/degel-clojure-utils "0.1.21"]]
   
-  :plugins [[lein-cljsbuild "1.0.2"]]
+  :plugins [[lein-cljsbuild "1.0.2"]
+            #_[lein-cljsbuild "1.0.3"]]
 
   :hooks [leiningen.cljsbuild]
 

103d5f7e522726d7f8d9a9801f26e97b1e6b481c
David Goldfarb <deg@degel.com>
2014-05-01 20:07:51 +0300

    Start reducing code.  Remove numbers.cljs and a large related swath of core.cljs

--- src/cljs/primes/core.cljs
+++ src/cljs/primes/core.cljs
@@ -10,101 +10,26 @@
 ;;; You must not remove this notice, or any other, from this software.
 
 
-;;; Toy program to play with the om library and reactive ClojureScript
-;;; programming.
-;;;
-;;; This is playing with an idea I had the other day of a new way to
-;;; visualize the natural numbers. Imagine that each number is
-;;; expressed as a vector in a multi-dimensional space where each
-;;; dimension corresponds with a prime number and the number's
-;;; magnitude in each dimension how many times that prime appears in
-;;; the numbers's prime factorization.
-;;;
-;;; So, 1 is at the origin, and higher powers of primes are further
-;;; aways from the origin. Expressed as vectors in this multi-dimensional
-;;; space, some small numbers look like:
-;;;
-;;;   2 - [1, 0, 0, 0, 0, ...]
-;;;   3 - [0, 1, 0, 0, 0, ...]
-;;;   4 - [2, 0, 0, 0, 0, ...]
-;;;   5 - [0, 0, 1, 0, 0, ...]
-;;;   6 - [1, 1, 0, 0, 0, ...]
-;;;   8 - [3, 0, 0, 0, 0, ...]
-;;;  10 - [1, 0, 1, 0, 0, ...]
-;;;  30 - [1, 1, 1, 0, 0, ...]
-;;;  36 - [2, 2, 0, 0, 0, ...]
-;;; 360 - [3, 2, 1, 0, 0, ...]
-;;;
-;;;
-;;; This is possibly interesting, but pretty much impossible to
-;;; visualize in any useful way. The number of dimensions just grows
-;;; way too fast to draw on two-dimensional paper; certainly way too
-;;; fast for my limited artistic abilities.
-;;;
-;;; But, let's look at things a different way. For each number, let's
-;;; look at each of the dimensions that have a non-zero magnitude
-;;; (that is, each of the prime factors), ignore order or value of the
-;;; primes, and use these "signatures" as buckets to group all the
-;;; natural numbers into distinct sets.
-;;;
-;;; So, prime numbers are in the set {1}; squares of primes are in
-;;; {2}; products of two distint primes are {1, 1}; etc.
-;;;
-;;; For clarity, let's look again at the numbers above:
-;;;
-;;;   2 - {1}
-;;;   3 - {1}
-;;;   4 - {2}
-;;;   5 - {1}
-;;;   6 - {1, 1}
-;;;   8 - {3}
-;;;  10 - {1, 1}
-;;;  30 - {1, 1, 1}
-;;;  36 - {2, 2}
-;;; 360 - {3, 2, 1}
-;;;
-;;; Now, an interesting question, and one that is relatively easy to
-;;; visualize, is how fast each of these sets grow. We know that the
-;;; number of primes less than N is approximately N/log(N), so that
-;;; gives us the growth rate of set {1}. But, I know very little about
-;;; the properites of the other sets. So, the goal of this exercise it
-;;; to create a web page with some useful visualizations.
-
-
 (ns primes.core.core
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [clojure.data :as data]
             [clojure.string :as str]
             [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [cljs.core.async :refer [put! chan <!]]
-            [degel.primes.numbers :as nums]))
+            [cljs.core.async :refer [put! chan <!]]))
 
 (enable-console-print!)
 
 
 (def app-state (atom {:title "Integer composite groups"
                       :up-to 10
-                      :groups (nums/group-signatures 10)}))
+                      :groups '([(1) (2 3 5 7 11 13)]
+                                [(1 1) (6 10 14)]
+                                [(2) (4 9)]
+                                [(2 1) (12)]
+                                [(3) (8)])}))
 
 
-(defn title [app]
-  (om/component (dom/h2 #js {:className "title"} (:title app))))
-
-
-(defn add-more
-  "Extend the range of numbers we are looking at"
-  [app]
-  (om/transact! app :up-to #(nums/ceil (* % 1.5)))
-  (om/update! app :groups (nums/group-signatures (:up-to @app))))
-
-
-(defn numseq [app]
-  (om/component
-   (dom/div #js {:className "numseq"}
-            "Up to: " (:up-to app) "  "
-            (dom/button #js {:onClick #(add-more app)}
-                        "show more"))))
 
 (defn counter-view [app owner]
   (reify
@@ -131,7 +56,7 @@
     (render-state [_ {:keys [expanded]}]
       (dom/li nil
               (dom/b nil (str/join ", " signature))
-              " [" (nums/vector-magnitude signature) "]"
+              " [" signature "]"
               (dom/br nil)
               (count numbers) ": "
               (dom/span #js {:onClick (fn [e] (om/set-state! owner :expanded (not expanded)))}
@@ -168,8 +93,6 @@
     om/IRenderState
     (render-state [this state]
       (dom/div nil
-               (om/build title app)
-               (om/build numseq app)
                (om/build counter-view app {})
                (dom/div nil "Uses wrapper, works")
                (dom/ul nil
